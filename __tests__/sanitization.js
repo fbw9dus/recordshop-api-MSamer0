931d3811e626e2205d54727dd9c1233390c21465
samer <samer@king.com>
2020-05-04 10:12:59 +0200

    Authentication

--- __tests__/sanitization.js
+++ __tests__/sanitization.js
@@ -6,22 +6,24 @@ const faker = require('faker')
 const validator = require('validator')
 
 let server;
+let token;
 
 describe('Sanitization', () => {
     test('email in db should be sanitized', async done => {
         ////// EMAIL
-        const unsanitizedEmail = 'Samer@joker.com'
-        const sanitizedEmail = validator.normalizeEmail(unsanitizedEmail)
-        console.log(sanitizedEmail)
+        
         const unsanitizedData = {
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
-            email: unsanitizedEmail,
-            password: faker.internet.password()
+            email: faker.internet.email().toUpperCase(),
+            password: faker.internet.password(),
+            role: 'User'
         }
+        const sanitizedEmail = validator.normalizeEmail(unsanitizedData.email)
         
         const res = await request(app)
             .post(`/users`)
+            .set('x-auth', `${token}`)
             .send(unsanitizedData)
         const checkUser = await User.findById(res.body._id)
         expect(checkUser.email).toBe(sanitizedEmail)
@@ -29,16 +31,18 @@ describe('Sanitization', () => {
     })
     test('firstname in db should be sanitized', async done => {
         ///// FIRSTNAME
-        const unsanitizedFirstName = 'Samer'
+        const unsanitizedFirstName = '  Maxim   '
         const sanitizedFirstName = validator.trim(unsanitizedFirstName)
         const unsanitizedData = {
             firstName: unsanitizedFirstName,
             lastName: faker.name.lastName(),
             email: faker.internet.email(),
-            password: faker.internet.password()
+            password: faker.internet.password(),
+            role: 'User'
         }
         const res = await request(app)
             .post(`/users`)
+            .set('x-auth', `${token}`)
             .send(unsanitizedData)
         const checkUser = await User.findById(res.body._id)
         expect(checkUser.firstName).toBe(sanitizedFirstName)
@@ -47,8 +51,26 @@ describe('Sanitization', () => {
 })
 
 beforeAll(async (done) => {
-    server = app.listen(3000, () => {
+    server = app.listen(3000, async () => {
         global.agent = request.agent(server);
+
+        //login
+        const fakeUser = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            role: "Admin"
+        }
+        let checkUser = await User.create(fakeUser)
+        //log in user
+        const login = await request(app)
+            .post(`/users/login`)
+            .send({
+                email: fakeUser.email,
+                password: fakeUser.password
+            })
+        token = login.header["x-auth"]
         done();
     });
 });
